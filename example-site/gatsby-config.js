514b43bf2f4fd5cf39f9192102d5e26291e00329
Ryan Krcil <ryan.krcil@carsforsale.com>
2021-07-24 13:27:22 -0500

    Implement gatsby image plugin

--- example-site/gatsby-config.js
+++ example-site/gatsby-config.js
@@ -1,14 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
   plugins: [
-    require.resolve(`../source-plugin`),
+    require.resolve(`../source-plugin`), 
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`,
   ],
 }
